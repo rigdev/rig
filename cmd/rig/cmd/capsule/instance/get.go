9046160f69606f2a413e5089bcccec1f4d7c9214
MatiasFrank <matias@rig.dev>
2023-10-03 17:17:25 +0200

    CLI: Use the new InstanceStatus in 'capsule instance get' (#157)

--- cmd/rig/cmd/capsule/instance/get.go
+++ cmd/rig/cmd/capsule/instance/get.go
@@ -1,73 +1,83 @@
 package instance
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/bufbuild/connect-go"
-	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/fatih/color"
 	"github.com/rigdev/rig-go-api/api/v1/capsule"
 	"github.com/rigdev/rig-go-api/model"
-	"github.com/rigdev/rig/cmd/common"
 	cmd_capsule "github.com/rigdev/rig/cmd/rig/cmd/capsule"
+	table2 "github.com/rodaine/table"
 	"github.com/spf13/cobra"
 )
 
 func (c Cmd) get(cmd *cobra.Command, args []string) error {
 	ctx := c.Ctx
-	resp, err := c.Rig.Capsule().ListInstances(ctx, &connect.Request[capsule.ListInstancesRequest]{
-		Msg: &capsule.ListInstancesRequest{
-			CapsuleId: cmd_capsule.CapsuleID,
-			Pagination: &model.Pagination{
-				Offset: uint32(offset),
-				Limit:  uint32(limit),
-			},
+	resp, err := c.Rig.Capsule().ListInstanceStatuses(ctx, connect.NewRequest(&capsule.ListInstanceStatusesRequest{
+		CapsuleId: cmd_capsule.CapsuleID,
+		Pagination: &model.Pagination{
+			Offset: uint32(offset),
+			Limit:  uint32(limit),
 		},
-	})
+	}))
 	if err != nil {
 		return err
 	}
-
 	instances := resp.Msg.GetInstances()
 
-	if len(args) > 0 {
-		found := false
-		for _, i := range instances {
-			if i.GetInstanceId() == args[0] {
-				instances = []*capsule.Instance{i}
-				found = true
-				break
-			}
-		}
-		if !found {
-			return fmt.Errorf("instance %q not found", args[0])
-		}
-	}
-
 	if outputJSON {
-		for _, i := range instances {
-			cmd.Println(common.ProtoToPrettyJson(i))
+		jsonStr, err := json.MarshalIndent(instances, "", "  ")
+		if err != nil {
+			return err
 		}
+		fmt.Println(string(jsonStr))
 		return nil
 	}
 
-	t := table.NewWriter()
-	t.AppendHeader(table.Row{fmt.Sprintf("Instances (%d)", resp.Msg.GetTotal()), "Build", "State", "Created At", "Uptime", "Restart Count"})
+	headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow).SprintfFunc()
+
+	tbl := table2.New("ID", "Scheduling", "Preparing", "Running")
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 	for _, i := range instances {
-		uptime := time.Since(i.GetStartedAt().AsTime())
-		if i.GetFinishedAt().AsTime().After(i.GetStartedAt().AsTime()) {
-			uptime = -time.Since(i.GetFinishedAt().AsTime())
+		for _, r := range instanceStatusToTableRows(i) {
+			tbl.AddRow(r...)
 		}
-		t.AppendRow(table.Row{
-			i.GetInstanceId(),
-			i.GetBuildId(),
-			i.GetState(),
-			i.GetCreatedAt().AsTime().Format(time.RFC3339),
-			uptime,
-			i.GetRestartCount(),
-		})
 	}
-	cmd.Println(t.Render())
+	tbl.Print()
 
 	return nil
+}
+
+func instanceStatusToTableRows(instance *capsule.InstanceStatus) [][]any {
+	rows := [][]any{
+		{"", "", "", ""},
+		{"", "", "", ""},
+	}
+	rows[0][0] = "id"
+
+	schedule := instance.GetSchedule()
+	rows[0][1] = capsule.ScheduleState_name[int32(schedule.GetState())]
+	rows[1][1] = schedule.GetMessage()
+
+	pulling := instance.GetPreparing().GetPulling()
+	rows[0][2] = capsule.ImagePullingState_name[int32(pulling.GetState())]
+	rows[1][2] = pulling.GetMessage()
+
+	running := instance.GetRunning()
+	if crashLoop := running.GetCrashLoopBackoff(); crashLoop != nil {
+		rows[0][3] = "CRASH_LOOP"
+		rows[1][3] = crashLoop.GetMessage()
+	}
+	if ready := running.GetReady(); ready != nil {
+		rows[0][3] = capsule.InstanceRunningReadyState_name[int32(ready.GetState())]
+		rows[1][3] = running.GetMessage()
+	}
+	if running := running.GetRunning(); running != nil {
+		rows[0][3] = "RUNNING"
+	}
+
+	return rows
 }
