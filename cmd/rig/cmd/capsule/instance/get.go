e1e4367ae9f66c7b94030b6a6484a56281d603ff
Matias Frank Jensen <matias@rig.dev>
2023-10-13 14:17:57 +0200

    Added documentation to the new InstanceStatus data format

--- cmd/rig/cmd/capsule/instance/get.go
+++ cmd/rig/cmd/capsule/instance/get.go
@@ -3,11 +3,15 @@ package instance
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/fatih/color"
 	"github.com/rigdev/rig-go-api/api/v1/capsule"
+	"github.com/rigdev/rig-go-api/api/v1/capsule/instance"
 	"github.com/rigdev/rig-go-api/model"
 	cmd_capsule "github.com/rigdev/rig/cmd/rig/cmd/capsule"
+	table2 "github.com/rodaine/table"
 	"github.com/spf13/cobra"
 )
 
@@ -34,47 +38,36 @@ func (c Cmd) get(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	// TODO use the new InstanceStatus data format and fix this code
-
-	// headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
-	// columnFmt := color.New(color.FgYellow).SprintfFunc()
-	//
-	// tbl := table2.New("ID", "Scheduling", "Preparing", "Running")
-	// tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-	// for _, i := range instances {
-	// 	for _, r := range instanceStatusToTableRows(i) {
-	// 		tbl.AddRow(r...)
-	// 	}
-	// }
-	// tbl.Print()
+	headerFmt := color.New(color.FgBlue, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow).SprintfFunc()
+	tbl := table2.New("ID", "Scheduling", "Preparing", "Running")
+	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	for _, i := range instances {
+		for _, r := range instanceStatusToTableRows(i) {
+			tbl.AddRow(r...)
+		}
+	}
+	tbl.Print()
 
 	return nil
 }
 
-// func instanceStatusToTableRows(instance *capsule.InstanceStatus) [][]any {
-// 	rows := [][]any{
-// 		{"", "", "", ""},
-// 		{"", "", "", ""},
-// 	}
-// 	rows[0][0] = instance.GetData().GetInstanceId()
-//
-// 	schedule := instance.GetStages().GetSchedule()
-// 	rows[1][1] = schedule.GetMessage()
-//
-// 	pulling := instance.GetStages().GetPreparing().GetStages().GetPulling()
-// 	rows[1][2] = pulling.GetMessage()
-//
-// 	running := instance.GetStages().GetRunning()
-// 	if crashLoop := running.GetStages().GetCrashLoopBackoff(); crashLoop != nil {
-// 		rows[0][3] = "CRASH_LOOP"
-// 		rows[1][3] = crashLoop.GetMessage()
-// 	}
-// 	if ready := running.GetStages().GetReady(); ready != nil {
-// 		rows[1][3] = running.GetMessage()
-// 	}
-// 	if running := running.GetStages().GetRunning(); running != nil {
-// 		rows[0][3] = "RUNNING"
-// 	}
-//
-// 	return rows
-// }
+func instanceStatusToTableRows(instance *instance.Status) [][]any {
+	rowLength := len(instance.GetStateMachine().GetStates()) + 1
+	rows := [][]any{
+		make([]any, rowLength),
+		make([]any, rowLength),
+	}
+
+	rows[0][0] = instance.GetInstanceId()
+	for idx, s := range instance.GetStateMachine().GetStates() {
+		m := s.GetSubStateMachines()[0]
+		s = m.GetStates()[0]
+		str := s.GetId().String()
+		str, _ = strings.CutPrefix(str, "STATE_ID_")
+		rows[0][idx+1] = str
+		rows[1][idx+1] = s.GetMessage()
+	}
+
+	return rows
+}
