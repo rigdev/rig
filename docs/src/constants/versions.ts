891ae83d9694b270edf88c7c65c593e3e4942236
Anders Johnsen <anders@rig.dev>
2023-12-21 09:41:14 +0100

    🔖 Tag new releases

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "v1.5.4";
-export const RIG_OPERATOR_CHART_VERSION = "0.1.27";
-export const RIG_PLATFORM_CHART_VERSION = "0.2.23";
+export const RIG_VERSION = "v1.5.5";
+export const RIG_OPERATOR_CHART_VERSION = "0.1.28";
+export const RIG_PLATFORM_CHART_VERSION = "0.2.24";
