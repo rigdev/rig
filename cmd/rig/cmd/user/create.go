c3b5a9edd0cd975c8f5e3fc8e9ae4f52b4772df5
Christian Leth-Espensen <christian@munklinde.dk>
2024-01-03 11:37:34 +0300

    add-group-role-step (#448)

--- cmd/rig/cmd/user/create.go
+++ cmd/rig/cmd/user/create.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"connectrpc.com/connect"
+	"github.com/rigdev/rig-go-api/api/v1/group"
 	"github.com/rigdev/rig-go-api/api/v1/user"
 	"github.com/rigdev/rig/cmd/common"
 	"github.com/spf13/cobra"
@@ -24,6 +25,28 @@ func (c *Cmd) create(ctx context.Context, cmd *cobra.Command, _ []string) error 
 	}
 
 	cmd.Println("User created with ID:", res.Msg.GetUser().GetUserId())
+
+	_, roleGroup, err := common.PromptSelect("What is the role of the user?",
+		[]string{"admin", "owner", "developer", "viewer"})
+	if err != nil {
+		return err
+	}
+
+	_, err = c.Rig.Group().AddMember(ctx, &connect.Request[group.AddMemberRequest]{
+		Msg: &group.AddMemberRequest{
+			GroupId: roleGroup,
+			MemberIds: []*group.MemberID{
+				{
+					Kind: &group.MemberID_UserId{
+						UserId: res.Msg.GetUser().GetUserId(),
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
