7550209ac073aa9e072397ab0ddcd249f5a011d0
Matias Frank Jensen <matias@rig.dev>
2024-01-20 14:39:17 +0100

    :children_crossing: Improve output format of `rig capsule instance get`

--- cmd/rig/cmd/capsule/instance/get.go
+++ cmd/rig/cmd/capsule/instance/get.go
@@ -16,7 +16,6 @@ import (
 )
 
 func (c *Cmd) get(ctx context.Context, _ *cobra.Command, _ []string) error {
-	// TODO Fix to use the new dataformat
 	resp, err := c.Rig.Capsule().ListInstanceStatuses(ctx, connect.NewRequest(&capsule.ListInstanceStatusesRequest{
 		CapsuleId: cmd_capsule.CapsuleID,
 		Pagination: &model.Pagination{
@@ -42,39 +41,34 @@ func (c *Cmd) get(ctx context.Context, _ *cobra.Command, _ []string) error {
 	tbl := table2.New("ID", "Scheduling", "Preparing", "Running", "Deleted")
 	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 	for _, i := range instances {
-		for _, r := range instanceStatusToTableRows(i) {
-			tbl.AddRow(r...)
-		}
+		tbl.AddRow(instanceStatusToTableRow(i)...)
 	}
 	tbl.Print()
 
 	return nil
 }
 
-func instanceStatusToTableRows(instance *instance.Status) [][]any {
-	rowLength := 5
-	rows := [][]any{
-		make([]any, rowLength),
-		make([]any, rowLength),
-	}
-
-	rows[0][0] = instance.GetInstanceId()
-	rows[1][0] = ""
+func instanceStatusToTableRow(instance *instance.Status) []any {
 	stages := instance.GetStages()
+	return []any{
+		instance.GetInstanceId(),
+		formatRow(stages.GetSchedule()),
+		formatRow(stages.GetPreparing()),
+		formatRow(stages.GetRunning()),
+		formatRow(stages.GetDeleted()),
+	}
+}
 
-	rows[0][1] = stages.GetSchedule().GetInfo().GetName()
-	rows[1][1] = formatStageState(stages.GetSchedule().GetInfo().GetState())
+type stage interface {
+	GetInfo() *instance.StageInfo
+}
 
-	rows[0][2] = stages.GetPreparing().GetInfo().GetName()
-	rows[1][2] = formatStageState(stages.GetPreparing().GetInfo().GetState())
-
-	rows[0][3] = stages.GetRunning().GetInfo().GetName()
-	rows[1][3] = formatStageState(stages.GetRunning().GetInfo().GetState())
-
-	rows[0][4] = stages.GetDeleted().GetInfo().GetName()
-	rows[1][4] = formatStageState(stages.GetDeleted().GetInfo().GetState())
-
-	return rows
+func formatRow(stage stage) string {
+	info := stage.GetInfo()
+	if info.GetState() == instance.StageState_STAGE_STATE_UNSPECIFIED {
+		return ""
+	}
+	return formatStageState(info.GetState())
 }
 
 func formatStageState(s instance.StageState) string {
