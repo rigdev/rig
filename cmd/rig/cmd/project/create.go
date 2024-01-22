ce38e08f48a6e6adb65da6d71116fe204004acfe
Matias Frank Jensen <matias@rig.dev>
2024-01-22 14:35:54 +0100

    fix

--- cmd/rig/cmd/project/create.go
+++ cmd/rig/cmd/project/create.go
@@ -6,6 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func (c *Cmd) create(ctx context.Context, cmd *cobra.Command, _ []string) error {
+func (c *Cmd) create(ctx context.Context, _ *cobra.Command, _ []string) error {
 	return c.Auth.CreateProject(ctx, name, &useProject)
 }
