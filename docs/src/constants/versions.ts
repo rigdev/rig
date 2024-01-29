816198993d8fd4e8a58372673cced2617426b3a9
MatiasFrank <matias@rig.dev>
2024-01-29 10:51:17 +0100

    Bump versions (#552)

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "v1.6.0";
-export const RIG_OPERATOR_CHART_VERSION = "0.1.30";
-export const RIG_PLATFORM_CHART_VERSION = "0.2.31";
+export const RIG_VERSION = "v1.6.1";
+export const RIG_OPERATOR_CHART_VERSION = "0.1.31";
+export const RIG_PLATFORM_CHART_VERSION = "0.2.32";
