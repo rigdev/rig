8caf551d2e8adf720b426f1c97aa3687621c603b
Christian Leth-Espensen <christian@munklinde.dk>
2024-02-09 11:36:48 +0300

    add environmentID to abort-rollout-request (#599)

--- cmd/rig/cmd/capsule/root/abort.go
+++ cmd/rig/cmd/capsule/root/abort.go
@@ -23,9 +23,10 @@ func (c *Cmd) abort(ctx context.Context, cmd *cobra.Command, _ []string) error {
 
 	if _, err := c.Rig.Capsule().AbortRollout(ctx, &connect.Request[capsule.AbortRolloutRequest]{
 		Msg: &capsule.AbortRolloutRequest{
-			CapsuleId: capsule_cmd.CapsuleID,
-			RolloutId: cc.Msg.GetCapsule().GetCurrentRollout(),
-			ProjectId: flags.GetProject(c.Cfg),
+			CapsuleId:     capsule_cmd.CapsuleID,
+			RolloutId:     cc.Msg.GetCapsule().GetCurrentRollout(),
+			ProjectId:     flags.GetProject(c.Cfg),
+			EnvironmentId: flags.GetEnvironment(c.Cfg),
 		},
 	}); err != nil {
 		return err
