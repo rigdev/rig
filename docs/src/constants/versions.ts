cfbaf3d1423f3038dd22386026b301e9fd298503
Anders Johnsen <anders@rig.dev>
2024-03-11 12:26:48 +0100

    🔖 Tag new operator version (#727)

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "v1.7.2";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.1";
+export const RIG_VERSION = "v1.7.3";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.2";
 export const RIG_PLATFORM_CHART_VERSION = "1.0.1";
