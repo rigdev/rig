e33e12656b735d4a847249ca0e65f22136127b54
Matias Frank Jensen <matias@rig.dev>
2024-04-01 16:34:35 +0200

    :recycle: Slight refactor of setting up Cobra commands to follow the rest of the CLIs

--- cmd/rig-operator/apichecker/apichecker.go
+++ cmd/rig-operator/apichecker/apichecker.go
@@ -22,66 +22,12 @@ const (
 	flagNamespace = "namespace"
 )
 
-func CMD() *cobra.Command {
+func Setup(parent *cobra.Command) {
 	cmd := &cobra.Command{
 		Use:   "apicheck",
 		Short: "checks if rig-operator apis are ready",
 		Long:  "checks if rig-operator apis are ready by dry-running a Capsule creation",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			interval, err := cmd.Flags().GetDuration(flagInterval)
-			if err != nil {
-				return err
-			}
-			timeout, err := cmd.Flags().GetDuration(flagTimeout)
-			if err != nil {
-				return err
-			}
-			namespace, err := cmd.Flags().GetString(flagNamespace)
-			if err != nil {
-				return err
-			}
-
-			config, err := rest.InClusterConfig()
-			if err != nil {
-				config, err = clientcmd.BuildConfigFromFlags("", filepath.Join(os.Getenv("HOME"), ".kube", "config"))
-				if err != nil {
-					return fmt.Errorf("could not create kubernetes config: %w", err)
-				}
-			}
-
-			s := scheme.New()
-			c, err := client.New(config, client.Options{
-				Scheme: s,
-			})
-			if err != nil {
-				return fmt.Errorf("could not create k8s client for apicheck")
-			}
-			c = client.NewNamespacedClient(client.NewDryRunClient(c), namespace)
-
-			log := log.New(false)
-
-			checker := apichecker.New(c)
-			ctx, cancel := context.WithTimeout(cmd.Context(), timeout)
-			defer cancel()
-			ticker := time.NewTicker(interval)
-			var lastErr error
-			for {
-				log.Info("checking rig-operator api")
-				if err := checker.Check(cmd.Context()); err != nil {
-					log.WithValues("error", err).Info("rig-operator api is not ready")
-					lastErr = err
-				} else {
-					log.Info("rig-operator api is ready")
-					return nil
-				}
-				select {
-				case <-ctx.Done():
-					return lastErr
-				case <-ticker.C:
-					continue
-				}
-			}
-		},
+		RunE:  apicheck,
 	}
 
 	flags := cmd.Flags()
@@ -89,5 +35,61 @@ func CMD() *cobra.Command {
 	flags.Duration(flagTimeout, time.Minute*2, "timeout for when we should stop checking")
 	flags.String(flagNamespace, "rig-system", "namespace where Capsule resource creation will dryrun")
 
-	return cmd
+	parent.AddCommand(cmd)
+}
+
+func apicheck(cmd *cobra.Command, _ []string) error {
+	interval, err := cmd.Flags().GetDuration(flagInterval)
+	if err != nil {
+		return err
+	}
+	timeout, err := cmd.Flags().GetDuration(flagTimeout)
+	if err != nil {
+		return err
+	}
+	namespace, err := cmd.Flags().GetString(flagNamespace)
+	if err != nil {
+		return err
+	}
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		config, err = clientcmd.BuildConfigFromFlags("", filepath.Join(os.Getenv("HOME"), ".kube", "config"))
+		if err != nil {
+			return fmt.Errorf("could not create kubernetes config: %w", err)
+		}
+	}
+
+	s := scheme.New()
+	c, err := client.New(config, client.Options{
+		Scheme: s,
+	})
+	if err != nil {
+		return fmt.Errorf("could not create k8s client for apicheck")
+	}
+	c = client.NewNamespacedClient(client.NewDryRunClient(c), namespace)
+
+	log := log.New(false)
+
+	checker := apichecker.New(c)
+	ctx, cancel := context.WithTimeout(cmd.Context(), timeout)
+	defer cancel()
+	ticker := time.NewTicker(interval)
+	var lastErr error
+	for {
+		log.Info("checking rig-operator api")
+		if err := checker.Check(cmd.Context()); err != nil {
+			log.WithValues("error", err).Info("rig-operator api is not ready")
+			lastErr = err
+		} else {
+			log.Info("rig-operator api is ready")
+			return nil
+		}
+		select {
+		case <-ctx.Done():
+			return lastErr
+		case <-ticker.C:
+			continue
+		}
+	}
 }
--- cmd/rig-operator/certgen/patch.go
+++ cmd/rig-operator/certgen/patch.go
@@ -17,102 +17,81 @@ const (
 	flagWebhookCFGName  = "webhook-cfg-name"
 )
 
-func patchCMD() (*cobra.Command, error) {
-	cmd := &cobra.Command{
-		Use:   "patch",
-		Args:  cobra.ExactArgs(0),
-		Short: "Patch validating/mutating webhook configurations and CRDs",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			flags := cmd.Flags()
-			webhookCFGName, err := flags.GetString(flagWebhookCFGName)
-			if err != nil {
-				return err
-			}
-			patchValidating, err := flags.GetBool(flagValidating)
-			if err != nil {
-				return err
-			}
-			patchMutating, err := flags.GetBool(flagMutating)
-			if err != nil {
-				return err
-			}
-			patchCRDs, err := flags.GetBool(flagCRDs)
-			if err != nil {
-				return err
-			}
-			secretName, err := flags.GetString(flagSecretName)
-			if err != nil {
-				return err
-			}
-			secretNamespace, err := flags.GetString(flagSecretNamespace)
-			if err != nil {
-				return err
-			}
-
-			k8s, err := newK8s()
-			if err != nil {
-				return err
-			}
-
-			log := log.New(false).WithValues(
-				"validating", patchValidating,
-				"mutating", patchMutating,
-				"crds", patchCRDs,
-				"secretName", secretName,
-				"secretNamespace", secretNamespace,
-				"name", webhookCFGName,
-			)
-
-			log.Info("getting certificate secret...")
-			s, err := k8s.getSecret(cmd.Context(), secretNamespace, secretName)
-			if err != nil {
-				return fmt.Errorf("could not get secret: %w", err)
-			}
-
-			ca := s.Data["ca.crt"]
-			if ca == nil {
-				return fmt.Errorf("secret %s/%s does not contain ca", secretNamespace, secretName)
-			}
-
-			log.Info("found certificate secret")
-			if patchValidating && webhookCFGName != "" {
-				log.Info("patching validating")
-				if err := k8s.patchValidating(cmd.Context(), webhookCFGName, ca); err != nil {
-					return err
-				}
-				log.Info("patched validating")
-			}
-			if patchMutating && webhookCFGName != "" {
-				log.Info("patching mutating")
-				if err := k8s.patchMutating(cmd.Context(), webhookCFGName, ca); err != nil {
-					return err
-
-				}
-				log.Info("patched mutating")
-			}
-			if patchCRDs {
-				log.Info("patching Capsule CRD")
-				if err := k8s.patchCRD(cmd.Context(), "capsules.rig.dev", ca); err != nil {
-					return err
-				}
-				log.Info("patched Capsule CRD")
-			}
-
-			return nil
-		},
+func patch(cmd *cobra.Command, args []string) error {
+	flags := cmd.Flags()
+	webhookCFGName, err := flags.GetString(flagWebhookCFGName)
+	if err != nil {
+		return err
+	}
+	patchValidating, err := flags.GetBool(flagValidating)
+	if err != nil {
+		return err
+	}
+	patchMutating, err := flags.GetBool(flagMutating)
+	if err != nil {
+		return err
+	}
+	patchCRDs, err := flags.GetBool(flagCRDs)
+	if err != nil {
+		return err
+	}
+	secretName, err := flags.GetString(flagSecretName)
+	if err != nil {
+		return err
+	}
+	secretNamespace, err := flags.GetString(flagSecretNamespace)
+	if err != nil {
+		return err
 	}
 
-	flags := cmd.PersistentFlags()
-	flags.Bool(flagValidating, true, "wether to patch ValidatingWebhookConfiguration with given name")
-	flags.Bool(flagMutating, true, "wether to patch MutatingWebhookConfiguration with given name")
-	flags.Bool(flagCRDs, true, "wether to patch CRDs")
-	flags.String(flagWebhookCFGName, "", "Name of *WebhookConfiguration resources")
-	flags.String(flagSecretName, "", "Name of certificate secret containing the ca")
-	flags.String(flagSecretNamespace, "default", "Namespace of certificate secret containing the ca")
-
-	if err := cobra.MarkFlagRequired(flags, flagSecretName); err != nil {
-		return nil, err
+	k8s, err := newK8s()
+	if err != nil {
+		return err
 	}
 
-	return cmd, nil
+	log := log.New(false).WithValues(
+		"validating", patchValidating,
+		"mutating", patchMutating,
+		"crds", patchCRDs,
+		"secretName", secretName,
+		"secretNamespace", secretNamespace,
+		"name", webhookCFGName,
+	)
+
+	log.Info("getting certificate secret...")
+	s, err := k8s.getSecret(cmd.Context(), secretNamespace, secretName)
+	if err != nil {
+		return fmt.Errorf("could not get secret: %w", err)
+	}
+
+	ca := s.Data["ca.crt"]
+	if ca == nil {
+		return fmt.Errorf("secret %s/%s does not contain ca", secretNamespace, secretName)
+	}
+
+	log.Info("found certificate secret")
+	if patchValidating && webhookCFGName != "" {
+		log.Info("patching validating")
+		if err := k8s.patchValidating(cmd.Context(), webhookCFGName, ca); err != nil {
+			return err
+		}
+		log.Info("patched validating")
+	}
+	if patchMutating && webhookCFGName != "" {
+		log.Info("patching mutating")
+		if err := k8s.patchMutating(cmd.Context(), webhookCFGName, ca); err != nil {
+			return err
+
+		}
+		log.Info("patched mutating")
+	}
+	if patchCRDs {
+		log.Info("patching Capsule CRD")
+		if err := k8s.patchCRD(cmd.Context(), "capsules.rig.dev", ca); err != nil {
+			return err
+		}
+		log.Info("patched Capsule CRD")
+	}
+
+	return nil
 }
