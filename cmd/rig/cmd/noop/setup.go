cc485228c1adf406489fef06c6bdfc23a2a588fc
Matias Frank Jensen <matias@rig.dev>
2024-04-09 20:33:32 +0200

    :white_check_mark: Add testing framework + some tests for the CLI
    The unit tests are meant to test the setup of the Rig CLI where we
    read in config, contexts, prompt for information and fetch
    project/capsule/environment.
    
    More tests to come.

---
+++ cmd/rig/cmd/noop/setup.go
@@ -0,0 +1,55 @@
+package noop
+
+import (
+	"github.com/rigdev/rig-go-sdk"
+	"github.com/rigdev/rig/cmd/common"
+	"github.com/rigdev/rig/cmd/rig/services/auth"
+	"github.com/rigdev/rig/pkg/cli"
+	"github.com/spf13/cobra"
+	"go.uber.org/fx"
+)
+
+type Cmd struct {
+	fx.In
+
+	Rig      rig.Client
+	Prompter common.Prompter
+}
+
+var cc Cmd
+
+func initCmd(c Cmd) {
+	cc = c
+}
+
+func Setup(parent *cobra.Command, s *cli.SetupContext) {
+	cmd := &cobra.Command{
+		Use:               "noop",
+		PersistentPreRunE: s.MakeInvokePreRunE(initCmd),
+	}
+	cmd1 := &cobra.Command{
+		Use:               "cmd1",
+		PersistentPreRunE: s.MakeInvokePreRunE(initCmd),
+		Annotations: map[string]string{
+			auth.OmitUser:        "",
+			auth.OmitEnvironment: "",
+			auth.OmitCapsule:     "",
+			auth.OmitProject:     "",
+		},
+		RunE: cc.noop,
+	}
+	cmd.AddCommand(cmd1)
+
+	cmd2 := &cobra.Command{
+		Use:               "cmd2",
+		PersistentPreRunE: s.MakeInvokePreRunE(initCmd),
+		RunE:              cc.noop,
+	}
+	cmd.AddCommand(cmd2)
+
+	parent.AddCommand(cmd)
+}
+
+func (c *Cmd) noop(_ *cobra.Command, _ []string) error {
+	return nil
+}
