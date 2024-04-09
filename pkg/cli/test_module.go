130d21035e7fb798c5e122496f251ec83d866970
Matias Frank Jensen <matias@rig.dev>
2024-04-09 11:09:20 +0200

    fix mocsk

--- pkg/cli/test_module.go
+++ pkg/cli/test_module.go
@@ -2,12 +2,10 @@ package cli
 
 import (
 	"context"
-	"testing"
 
 	"github.com/docker/docker/client"
 	"github.com/rigdev/rig-go-sdk"
 	"github.com/rigdev/rig/cmd/rig/cmd/cmdconfig"
-	mockrig "github.com/rigdev/rig/gen/uncommittedmocks/github.com/rigdev/rig-go-sdk"
 	"github.com/rigdev/rig/pkg/cli/scope"
 	"github.com/rigdev/rig/pkg/scheme"
 	"github.com/spf13/afero"
@@ -15,14 +13,16 @@ import (
 	"go.uber.org/zap"
 )
 
-func MakeTestModule(t *testing.T, isInteractive bool) fx.Option {
+type TestModuleInput struct {
+	RigClient     rig.Client
+	IsInteractive bool
+}
+
+func MakeTestModule(i TestModuleInput) fx.Option {
 	return fx.Module(
 		"test-rig-cli",
-		fx.Provide(func() *testing.T {
-			return t
-		}),
-		fx.Provide(func(t *testing.T) rig.Client {
-			return mockrig.NewMockClient(t)
+		fx.Provide(func() rig.Client {
+			return i.RigClient
 		}),
 		fx.Provide(scheme.New),
 		fx.Provide(func(fs afero.Fs) (*cmdconfig.Config, error) {
@@ -37,7 +37,7 @@ func MakeTestModule(t *testing.T, isInteractive bool) fx.Option {
 		}),
 		fx.Provide(func() *PromptInformation { return &PromptInformation{} }),
 		fx.Provide(func() scope.Interactive {
-			return scope.Interactive(isInteractive)
+			return scope.Interactive(i.IsInteractive)
 		}),
 	)
 }
