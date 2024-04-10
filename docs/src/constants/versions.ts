2118d392fd09e1baa6f903104dd6c91ff87c30b0
Anders Johnsen <anders@rig.dev>
2024-04-10 12:24:48 +0200

    more versions

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
 export const RIG_VERSION = "1.8.11";
 export const RIG_OPERATOR_CHART_VERSION = "1.0.14";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.15";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.16";
