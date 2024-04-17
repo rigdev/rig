1ef645fce1a00aba3af89c2d16574d80d602e71e
MatiasFrank <matias@rig.dev>
2024-04-17 12:42:11 +0200

    :sparkles: Add ListExisting and ListNew to the plugin PipelineRequest (#869)

--- pkg/controller/pipeline/capsule_request_test.go
+++ pkg/controller/pipeline/capsule_request_test.go
@@ -10,13 +10,20 @@ import (
 	"github.com/rigdev/rig/pkg/api/v1alpha2"
 	"github.com/rigdev/rig/pkg/scheme"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func preparePipelineTest(t *testing.T, opts ...CapsuleRequestOption) (
+type pipelineTestOpts struct {
+	options         []CapsuleRequestOption
+	existingObjects []client.Object
+}
+
+func preparePipelineTest(t *testing.T, options pipelineTestOpts) (
 	context.Context, *mockclient.MockClient, *capsuleRequest,
 ) {
 	scheme := scheme.New()
@@ -24,13 +31,20 @@ func preparePipelineTest(t *testing.T, opts ...CapsuleRequestOption) (
 	ctx := context.Background()
 
 	p := NewCapsulePipeline(cc, cc, &v1alpha1.OperatorConfig{}, scheme, logr.Discard())
-	c := newCapsuleRequest(p, &v1alpha2.Capsule{}, opts...)
+	c := newCapsuleRequest(p, &v1alpha2.Capsule{}, options.options...)
+	for _, obj := range options.existingObjects {
+		key, err := c.GetKey(obj)
+		require.NoError(t, err)
+		c.existingObjects[key] = obj
+	}
 
 	return ctx, cc, c
 }
 
 func TestOverrideUntrackedWithForceGivesAborted(t *testing.T) {
-	ctx, cc, c := preparePipelineTest(t, WithForce())
+	ctx, cc, c := preparePipelineTest(t, pipelineTestOpts{
+		options: []CapsuleRequestOption{WithForce()},
+	})
 
 	sa := &v1.ServiceAccount{}
 	sa.SetName("test")
@@ -47,7 +61,7 @@ func TestOverrideUntrackedWithForceGivesAborted(t *testing.T) {
 }
 
 func TestOverrideUntrackedWithoutForceGivesNoop(t *testing.T) {
-	ctx, cc, c := preparePipelineTest(t)
+	ctx, cc, c := preparePipelineTest(t, pipelineTestOpts{})
 
 	sa := &v1.ServiceAccount{}
 	sa.SetName("test")
@@ -67,4 +81,53 @@ func TestOverrideUntrackedWithoutForceGivesNoop(t *testing.T) {
 			state: ResourceStateAlreadyExists,
 		},
 	}, cs)
+}
+
+func Test_ListExisting(t *testing.T) {
+	_, _, c := preparePipelineTest(t, pipelineTestOpts{
+		existingObjects: []client.Object{
+			&corev1.Service{ObjectMeta: metav1.ObjectMeta{
+				Name: "service1",
+			}},
+			&corev1.Service{ObjectMeta: metav1.ObjectMeta{
+				Name: "service2",
+			}},
+			&corev1.ServiceAccount{ObjectMeta: metav1.ObjectMeta{
+				Name: "serviceaccount",
+			}},
+		},
+	})
+	services, err := ListExisting(c, &corev1.Service{})
+	require.NoError(t, err)
+	require.Equal(t, []*corev1.Service{
+		{ObjectMeta: metav1.ObjectMeta{Name: "service1"}},
+		{ObjectMeta: metav1.ObjectMeta{Name: "service2"}},
+	}, services)
+}
+
+func Test_ListNew(t *testing.T) {
+	_, _, c := preparePipelineTest(t, pipelineTestOpts{})
+
+	require.NoError(t, c.Set(&corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "service1",
+		},
+	}))
+	require.NoError(t, c.Set(&corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "service2",
+		},
+	}))
+	require.NoError(t, c.Set(&corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "serviceaccount",
+		},
+	}))
+
+	services, err := ListNew(c, &corev1.Service{})
+	require.NoError(t, err)
+	require.Equal(t, []*corev1.Service{
+		{ObjectMeta: metav1.ObjectMeta{Name: "service1"}},
+		{ObjectMeta: metav1.ObjectMeta{Name: "service2"}},
+	}, services)
 }
