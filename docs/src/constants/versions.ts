4df029b4fe56ad9e87b7177f7ea584cf0ca01939
Anders Johnsen <anders@rig.dev>
2024-04-24 17:14:44 +0200

    🔖 Tag new platform version (#897)

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
 export const RIG_VERSION = "1.8.17";
 export const RIG_OPERATOR_CHART_VERSION = "1.0.17";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.20";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.21";
