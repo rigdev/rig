0a03603527606dca4a2d777bf3c8863599faa103
MatiasFrank <matias@rig.dev>
2024-05-23 14:11:00 +0200

    :memo: Tag new chart versions (#967)

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
 export const RIG_VERSION = "1.9.3";
 export const RIG_OPERATOR_CHART_VERSION = "1.0.21";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.24";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.25";
