dc12f20c32a5c328e1bf5e489aa719a02d65787e
Christian Munklinde Leth-Espensen <christian@munklinde.dk>
2024-05-27 17:09:24 +0200

    :bookmark: platform chart version

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
 export const RIG_VERSION = "1.9.5";
 export const RIG_OPERATOR_CHART_VERSION = "1.0.23";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.26";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.27";
