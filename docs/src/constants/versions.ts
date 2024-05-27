8eb2d39b5c70006ede7287529eb0ec5589e78636
Christian Leth-Espensen <christian@munklinde.dk>
2024-05-27 16:13:25 +0200

    :bookmark: Update versions in docs (#979)

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "1.9.3";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.21";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.25";
+export const RIG_VERSION = "1.9.4";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.22";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.26";
