d5c87637325bc30152eb9b8d829a1cc4e3c631ce
Matias Frank Jensen <matias@rig.dev>
2024-05-28 08:12:40 +0200

    :memo: Tag new release

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "1.9.4";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.22";
+export const RIG_VERSION = "1.9.5";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.23";
 export const RIG_PLATFORM_CHART_VERSION = "1.0.26";
