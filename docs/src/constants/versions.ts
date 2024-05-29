4a9c1d939890aad256c0dc546c4d084d1c81dd8b
Christian Munklinde Leth-Espensen <christian@munklinde.dk>
2024-05-29 12:12:21 +0200

    :bookmark: New appversions in charts

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "1.9.5";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.23";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.27";
+export const RIG_VERSION = "1.9.6";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.24";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.28";
