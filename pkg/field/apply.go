a2c7af3d2b8839a05d8e95d959772d4fd8d170bf
Christian Leth-Espensen <christian@munklinde.dk>
2024-07-10 12:23:45 +0200

    :bug: Finished the field pkg (#1094)

--- pkg/field/apply.go
+++ pkg/field/apply.go
@@ -3,18 +3,18 @@ package field
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 	"text/scanner"
 	"unicode"
 
 	"github.com/gonvenience/ytbx"
-	platformv1 "github.com/rigdev/rig-go-api/platform/v1"
 	"github.com/rigdev/rig/pkg/obj"
 	"google.golang.org/protobuf/proto"
 )
 
-func Apply(spec *platformv1.CapsuleSpec, changes []Change) (*platformv1.CapsuleSpec, error) {
-	out := proto.Clone(spec).(*platformv1.CapsuleSpec)
+func Apply(object proto.Message, changes []Change) (proto.Message, error) {
+	out := proto.Clone(object)
 
 	for _, change := range changes {
 		path, err := parseJSONPath(change.FieldPath)
@@ -26,18 +26,20 @@ func Apply(spec *platformv1.CapsuleSpec, changes []Change) (*platformv1.CapsuleS
 		var parent reflect.Value
 
 		lookupPath := path.PathElements
-		if change.Operation == AddedOperation {
-			lookupPath = lookupPath[:len(lookupPath)-1]
-		}
 
 		for _, pe := range lookupPath {
 			parent = value
 			switch value.Type().Kind() {
 			case reflect.Slice:
+				// If doing operation on an existing slice where we use the key to find the element
 				found := false
 				for i := range value.Len() { //nolint:typecheck
 					if pe.Key != "" {
-						elem := value.Index(i).Elem()
+						elem := value.Index(i)
+						// TODO: This is safe as of now, as we only support proto messages
+						if elem.Kind() == reflect.Ptr {
+							elem = elem.Elem()
+						}
 						field, err := findInStruct(elem, pe.Key)
 						if err != nil {
 							continue
@@ -51,9 +53,22 @@ func Apply(spec *platformv1.CapsuleSpec, changes []Change) (*platformv1.CapsuleS
 						}
 					}
 				}
-				if !found {
-					return nil, fmt.Errorf("invalid index in array")
+				if found {
+					continue
+				} else if !found && change.Operation == ModifiedOperation || change.Operation == RemovedOperation {
+					// If doing a modification or remove operation on existing slice where we find element using the index
+					if pe.Idx < 0 || pe.Idx >= value.Len() {
+						return nil, fmt.Errorf("invalid index in array")
+					}
+					value = value.Index(pe.Idx)
+					if value.Kind() == reflect.Ptr {
+						value = value.Elem()
+					}
+					continue
 				}
+
+				// If adding to a slice, create a reference to the new element
+				value = reflect.New(value.Type().Elem()).Elem()
 
 			case reflect.Struct:
 				field, err := findInStruct(value, pe.Name)
@@ -61,41 +76,72 @@ func Apply(spec *platformv1.CapsuleSpec, changes []Change) (*platformv1.CapsuleS
 					return nil, err
 				}
 
+				// TODO: This is safe as of now, as we only support proto messages - Revisit if it changes
 				if field.Kind() == reflect.Pointer {
-					field = field.Elem()
+					if field.IsNil() {
+						field.Set(reflect.New(field.Type().Elem()))
+					}
+					value = field.Elem()
+				} else {
+					value = field
 				}
-				value = field
 
 			case reflect.Map:
-				field := value.MapIndex(reflect.ValueOf(pe.Name))
-				if field == (reflect.Value{}) {
+				if value.IsNil() {
+					value.Set(reflect.MakeMap(value.Type()))
+				}
+
+				entry := value.MapIndex(reflect.ValueOf(pe.Name))
+				if entry == (reflect.Value{}) && change.Operation != AddedOperation {
 					return nil, fmt.Errorf("path '%s' not found", pe.Name)
 				}
 
-				// if field.Kind() == reflect.Pointer {
-				// 	field = field.Elem()
-				// }
-				value = field
-
+				value = reflect.New(value.Type().Elem()).Elem()
 			default:
-				fmt.Println(value.Type().Kind())
 				return nil, fmt.Errorf("invalid path navigation: %s", pe.Name)
 			}
 		}
 
 		switch change.Operation {
 		case ModifiedOperation:
-			newValue := reflect.New(value.Type())
+			var newValue reflect.Value
+			if value.Kind() == reflect.Ptr {
+				newValue = reflect.New(value.Type().Elem())
+			} else {
+				newValue = reflect.New(value.Type())
+			}
+
 			if err := obj.Decode([]byte(change.To.AsString), newValue.Interface()); err != nil {
 				return nil, err
 			}
+			switch parent.Type().Kind() {
+			case reflect.Map:
+				if newValue.Kind() == reflect.Ptr && parent.Type().Elem().Kind() != reflect.Ptr {
+					parent.SetMapIndex(reflect.ValueOf(lookupPath[len(lookupPath)-1].Name), newValue.Elem())
+				} else {
+					parent.SetMapIndex(reflect.ValueOf(lookupPath[len(lookupPath)-1].Name), newValue)
+				}
+			case reflect.Struct:
+				key := path.PathElements[len(path.PathElements)-1].Name
+				field, err := findInStruct(parent, key)
+				if err != nil {
+					return nil, err
+				}
 
-			if parent.Kind() == reflect.Map {
-				parent.SetMapIndex(reflect.ValueOf(lookupPath[len(lookupPath)-1].Name), newValue.Elem())
-			} else {
-				value.Set(newValue.Elem())
+				if newValue.Kind() == reflect.Ptr && field.Kind() != reflect.Ptr {
+					field.Set(newValue.Elem())
+				} else {
+					field.Set(newValue)
+				}
+			case reflect.Slice:
+				if newValue.Kind() == reflect.Ptr && value.Kind() != reflect.Ptr {
+					value.Set(newValue.Elem())
+				} else {
+					value.Set(newValue)
+				}
+			default:
+				return nil, fmt.Errorf("invalid assignment")
 			}
-
 		case RemovedOperation:
 			switch parent.Type().Kind() {
 			case reflect.Slice:
@@ -104,33 +150,71 @@ func Apply(spec *platformv1.CapsuleSpec, changes []Change) (*platformv1.CapsuleS
 					if elem == value {
 						nval := reflect.MakeSlice(parent.Type(), parent.Len()-1, parent.Len()-1)
 						reflect.Copy(nval, parent.Slice(0, i))
-						reflect.Copy(nval, parent.Slice(i, parent.Len()))
+						reflect.Copy(nval, parent.Slice(i+1, parent.Len()))
 						parent.Set(nval)
 						break
 					}
 				}
 
 			case reflect.Struct:
-			default:
-				return nil, fmt.Errorf("invalid asignment")
-			}
-
-		case AddedOperation:
-			switch value.Type().Kind() {
-			case reflect.Slice:
-				newValue := reflect.New(value.Type())
-				if err := obj.Decode([]byte(change.To.AsString), newValue.Interface()); err != nil {
+				field, err := findInStruct(parent, lookupPath[len(lookupPath)-1].Name)
+				if err != nil {
 					return nil, err
 				}
 
-				nval := reflect.MakeSlice(value.Type(), value.Len()+1, value.Len()+1)
-				reflect.Copy(nval, value)
-				nval.Index(value.Len()).Set(newValue.Elem().Index(0))
-				value.Set(nval)
-
-			case reflect.Struct:
+				field.Set(reflect.New(field.Type()).Elem())
+			case reflect.Map:
+				parent.SetMapIndex(reflect.ValueOf(lookupPath[len(lookupPath)-1].Name), reflect.Value{})
 			default:
-				return nil, fmt.Errorf("invalid asignment")
+				return nil, fmt.Errorf("invalid assignment")
+			}
+
+		case AddedOperation:
+			if value.Kind() == reflect.Ptr {
+				value = reflect.New(value.Type().Elem())
+			} else {
+				value = reflect.New(value.Type())
+			}
+
+			if err := obj.Decode([]byte(change.To.AsString), value.Interface()); err != nil {
+				return nil, err
+			}
+
+			switch parent.Type().Kind() {
+			case reflect.Slice:
+				// TODO: Add to the correct index if idx is provided
+				nval := reflect.MakeSlice(parent.Type(), parent.Len()+1, parent.Len()+1)
+				reflect.Copy(nval, parent)
+
+				if value.Kind() == reflect.Ptr && parent.Type().Elem().Kind() != reflect.Ptr {
+					nval.Index(parent.Len()).Set(value.Elem())
+				} else {
+					nval.Index(parent.Len()).Set(value)
+				}
+
+				parent.Set(nval)
+			case reflect.Struct:
+				key := path.PathElements[len(path.PathElements)-1].Name
+				field, err := findInStruct(parent, key)
+				if err != nil {
+					return nil, err
+				}
+
+				if value.Kind() == reflect.Ptr && field.Kind() != reflect.Ptr {
+					field.Set(value.Elem())
+				} else {
+					field.Set(value)
+				}
+			case reflect.Map:
+				key := path.PathElements[len(path.PathElements)-1].Name
+
+				if value.Kind() == reflect.Ptr && parent.Type().Elem().Kind() != reflect.Ptr {
+					parent.SetMapIndex(reflect.ValueOf(key), value.Elem())
+				} else {
+					parent.SetMapIndex(reflect.ValueOf(key), value)
+				}
+			default:
+				return nil, fmt.Errorf("invalid assignment")
 			}
 		}
 	}
@@ -159,7 +243,7 @@ func parseJSONPath(jsonPath string) (ytbx.Path, error) {
 		case ".":
 		case "$":
 		case "[":
-			s, err := parseJSONPathNamed(&sc)
+			s, err := parseJSONPathBracket(&sc)
 			if err != nil {
 				return ytbx.Path{}, err
 			}
@@ -176,11 +260,46 @@ func parseJSONPath(jsonPath string) (ytbx.Path, error) {
 	return result, nil
 }
 
-func parseJSONPathNamed(sc *scanner.Scanner) (ytbx.PathElement, error) {
-	if sc.Scan() != '@' {
-		return ytbx.PathElement{}, fmt.Errorf("invalid jsonpath")
+func parseJSONPathBracket(sc *scanner.Scanner) (ytbx.PathElement, error) {
+	sc.Scan()
+	t := sc.TokenText()
+	if t == "@" {
+		return parseJSONPathNamed(sc)
 	}
 
+	match := _indexRegexp.MatchString(t)
+	if match {
+		return parseJSONPathIndexed(sc)
+	}
+
+	return ytbx.PathElement{}, fmt.Errorf("invalid jsonpath")
+}
+
+func parseJSONPathIndexed(sc *scanner.Scanner) (ytbx.PathElement, error) {
+	index := sc.TokenText()
+	for {
+		switch sc.Scan() {
+		case ']':
+			i, err := strconv.Atoi(index)
+			if err != nil {
+				return ytbx.PathElement{}, err
+			}
+			return ytbx.PathElement{
+				Idx: i,
+			}, nil
+		default:
+			p := sc.TokenText()
+			match := _indexRegexp.MatchString(p)
+			if !match {
+				return ytbx.PathElement{}, fmt.Errorf("invalid jsonpath")
+			}
+
+			index += p
+		}
+	}
+}
+
+func parseJSONPathNamed(sc *scanner.Scanner) (ytbx.PathElement, error) {
 	sc.Scan()
 	name := sc.TokenText()
 
--- pkg/field/apply_test.go
+++ pkg/field/apply_test.go
@@ -9,13 +9,39 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func Test_Apply(t *testing.T) {
-	tests := []struct {
-		Name     string
-		Base     *platformv1.CapsuleSpec
-		Changes  []Change
-		Expected *platformv1.CapsuleSpec
-	}{
+type test struct {
+	Name     string
+	Base     *platformv1.CapsuleSpec
+	Changes  []Change
+	Expected *platformv1.CapsuleSpec
+}
+
+func Test_Apply_Interfaces(t *testing.T) {
+	tests := []test{
+		{
+			Name: "change interface port",
+			Base: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{{
+					Name: "foobar",
+					Port: 80,
+				}},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.interfaces[@port=80].port",
+					To: Value{
+						AsString: "81",
+					},
+					Operation: ModifiedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{{
+					Name: "foobar",
+					Port: 81,
+				}},
+			},
+		},
 		{
 			Name: "change interface name",
 			Base: &platformv1.CapsuleSpec{
@@ -65,9 +91,7 @@ func Test_Apply(t *testing.T) {
 				{
 					FieldPath: "$.interfaces[@port=80]",
 					To: Value{
-						AsString: `
-- name: foobar
-  port: 80`,
+						AsString: "name: foobar\nport: 80\n",
 					},
 					Operation: AddedOperation,
 				},
@@ -79,6 +103,179 @@ func Test_Apply(t *testing.T) {
 				}},
 			},
 		},
+		{
+			Name: "add multiple interfaces",
+			Base: &platformv1.CapsuleSpec{},
+			Changes: []Change{
+				{
+					FieldPath: "$.interfaces[@port=80]",
+					To: Value{
+						AsString: "name: foobar\nport: 80\n",
+					},
+					Operation: AddedOperation,
+				},
+				{
+					FieldPath: "$.interfaces[@port=81]",
+					To: Value{
+						AsString: "name: foobar2\nport: 81\n",
+					},
+					Operation: AddedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name: "foobar",
+						Port: 80,
+					},
+					{
+						Name: "foobar2",
+						Port: 81,
+					},
+				},
+			},
+		},
+	}
+
+	runTests(t, tests)
+}
+
+func Test_Apply_Routes(t *testing.T) {
+	tests := []test{
+		{
+			Name: "change route host",
+			Base: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name: "foobar",
+						Port: 80,
+						Routes: []*v1alpha2.HostRoute{
+							{
+								Id:   "foobarid",
+								Host: "example.com",
+							},
+						},
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.interfaces[@port=80].routes[@id=foobarid].host",
+					FieldID:   "$.interfaces[@port=80].routes[@id=foobarid].host",
+					From: Value{
+						AsString: "example.com",
+						Type:     StringType,
+					},
+					To: Value{
+						AsString: "example2.com",
+						Type:     StringType,
+					},
+					Operation: ModifiedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name: "foobar",
+						Port: 80,
+						Routes: []*v1alpha2.HostRoute{
+							{
+								Id:   "foobarid",
+								Host: "example2.com",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "Remove route",
+			Base: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name: "foobar",
+						Port: 80,
+						Routes: []*v1alpha2.HostRoute{
+							{
+								Id:   "foobarid",
+								Host: "example.com",
+							},
+						},
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.interfaces[@port=80].routes[@id=foobarid]",
+					FieldID:   "$.interfaces[@port=80].routes[@id=foobarid]",
+					From: Value{
+						AsString: "example.com",
+						Type:     StringType,
+					},
+					To: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					Operation: RemovedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name:   "foobar",
+						Port:   80,
+						Routes: []*v1alpha2.HostRoute{},
+					},
+				},
+			},
+		},
+		{
+			Name: "Add Route to existing interface",
+			Base: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name: "foobar",
+						Port: 80,
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.interfaces[@port=80].routes[@id=foobarid]",
+					FieldID:   "$.interfaces[@port=80].routes[@id=foobarid]",
+					From: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					To: Value{
+						AsString: "id: foobarid\nhost: example.com\n",
+						Type:     MapType,
+					},
+					Operation: AddedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Interfaces: []*v1alpha2.CapsuleInterface{
+					{
+						Name: "foobar",
+						Port: 80,
+						Routes: []*v1alpha2.HostRoute{
+							{
+								Id:   "foobarid",
+								Host: "example.com",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	runTests(t, tests)
+}
+
+func Test_Apply_Env_Vars(t *testing.T) {
+	tests := []test{
 		{
 			Name: "change environment variable",
 			Base: &platformv1.CapsuleSpec{
@@ -105,15 +302,390 @@ func Test_Apply(t *testing.T) {
 				},
 			},
 		},
+		{
+			Name: "add environment variable",
+			Base: &platformv1.CapsuleSpec{},
+			Changes: []Change{
+				{
+					FieldPath: "$.env.raw.key1",
+					To: Value{
+						AsString: "value1",
+						Type:     StringType,
+					},
+					Operation: AddedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Env: &platformv1.EnvironmentVariables{
+					Raw: map[string]string{
+						"key1": "value1",
+					},
+				},
+			},
+		},
+		{
+			Name: "add multiple environment variables",
+			Base: &platformv1.CapsuleSpec{},
+			Changes: []Change{
+				{
+					FieldPath: "$.env.raw.key1",
+					To: Value{
+						AsString: "value1",
+					},
+					Operation: AddedOperation,
+				},
+				{
+					FieldPath: "$.env.raw.key2",
+					To: Value{
+						AsString: "value2",
+					},
+					Operation: AddedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Env: &platformv1.EnvironmentVariables{
+					Raw: map[string]string{
+						"key1": "value1",
+						"key2": "value2",
+					},
+				},
+			},
+		},
+		{
+			Name: "remove environment variable",
+			Base: &platformv1.CapsuleSpec{
+				Env: &platformv1.EnvironmentVariables{
+					Raw: map[string]string{
+						"key1": "value1",
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.env.raw.key1",
+					Operation: RemovedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Env: &platformv1.EnvironmentVariables{
+					Raw: map[string]string{},
+				},
+			},
+		},
 	}
 
+	runTests(t, tests)
+}
+
+func Test_Apply_Primary_Fields(t *testing.T) {
+	tests := []test{
+		{
+			Name: "Change Image",
+			Base: &platformv1.CapsuleSpec{
+				Image: "foo/bar:latest",
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.image",
+					To: Value{
+						AsString: "foo/bar:1.0.0",
+					},
+					Operation: ModifiedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Image: "foo/bar:1.0.0",
+			},
+		},
+		{
+			Name: "Add image",
+			Base: &platformv1.CapsuleSpec{},
+			Changes: []Change{
+				{
+					FieldPath: "$.image",
+					To: Value{
+						AsString: "foo/bar:latest",
+					},
+					Operation: AddedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Image: "foo/bar:latest",
+			},
+		},
+		{
+			Name: "Remove image",
+			Base: &platformv1.CapsuleSpec{
+				Image: "foo/bar:latest",
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.image",
+					Operation: RemovedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{},
+		},
+		{
+			Name: "Change Command",
+			Base: &platformv1.CapsuleSpec{
+				Command: "echo",
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.command",
+					To: Value{
+						AsString: "ls",
+					},
+					Operation: ModifiedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Command: "ls",
+			},
+		},
+	}
+
+	runTests(t, tests)
+}
+
+func Test_Apply_Args(t *testing.T) {
+	tests := []test{
+		{
+			Name: "Change Args",
+			Base: &platformv1.CapsuleSpec{
+				Args: []string{"arg1"},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.args[0]",
+					From: Value{
+						AsString: "arg1",
+					},
+					To: Value{
+						AsString: "arg2",
+					},
+					Operation: ModifiedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Args: []string{"arg2"},
+			},
+		},
+		{
+			Name: "Add Args",
+			Base: &platformv1.CapsuleSpec{},
+			Changes: []Change{
+				{
+					FieldPath: "$.args.arg1",
+					To: Value{
+						AsString: `arg1`,
+					},
+					Operation: AddedOperation,
+				},
+				{
+					FieldPath: "args.arg2",
+					To: Value{
+						AsString: `arg2`,
+					},
+					Operation: AddedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Args: []string{"arg1", "arg2"},
+			},
+		},
+	}
+
+	runTests(t, tests)
+}
+
+func Test_Apply_Scale(t *testing.T) {
+	tests := []test{
+		{
+			Name: "Change Scale",
+			Base: &platformv1.CapsuleSpec{
+				Scale: &platformv1.Scale{
+					Horizontal: &platformv1.HorizontalScale{
+						Min: 1,
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.scale.horizontal.min",
+					To: Value{
+						AsString: "2",
+						Type:     StringType,
+					},
+					Operation: ModifiedOperation,
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Scale: &platformv1.Scale{
+					Horizontal: &platformv1.HorizontalScale{
+						Min: 2,
+					},
+				},
+			},
+		},
+		{
+			Name: "Add Horizontal Autoscaler",
+			Base: &platformv1.CapsuleSpec{
+				Scale: &platformv1.Scale{},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Scale: &platformv1.Scale{
+					Horizontal: &platformv1.HorizontalScale{
+						Min: 2,
+						Max: 10,
+						CpuTarget: &v1alpha2.CPUTarget{
+							Utilization: 80,
+						},
+						CustomMetrics: []*v1alpha2.CustomMetric{
+							{
+								InstanceMetric: &v1alpha2.InstanceMetric{
+									MetricName:   "metric1",
+									AverageValue: "10",
+								},
+							},
+						},
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.scale.horizontal.min",
+					From: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					To: Value{
+						AsString: "2",
+						Type:     OtherType,
+					},
+					Operation: AddedOperation,
+				},
+				{
+					FieldPath: "$.scale.horizontal.max",
+					From: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					To: Value{
+						AsString: "10",
+						Type:     OtherType,
+					},
+					Operation: AddedOperation,
+				},
+				{
+					FieldPath: "$.scale.horizontal.cpuTarget.utilization",
+					From: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					To: Value{
+						AsString: "80",
+						Type:     OtherType,
+					},
+					Operation: AddedOperation,
+				},
+				{
+					FieldPath: "$.scale.horizontal.customMetrics[0]",
+					From: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					To: Value{
+						AsString: "instanceMetric:\n  metricName: metric1\n  averageValue: 10\n",
+						Type:     OtherType,
+					},
+					Operation: AddedOperation,
+				},
+			},
+		},
+		{
+			Name: "Change second custom metrics",
+			Base: &platformv1.CapsuleSpec{
+				Scale: &platformv1.Scale{
+					Horizontal: &platformv1.HorizontalScale{
+						CustomMetrics: []*v1alpha2.CustomMetric{
+							{
+								InstanceMetric: &v1alpha2.InstanceMetric{
+									MetricName: "metric1",
+								},
+							},
+							{
+								InstanceMetric: &v1alpha2.InstanceMetric{
+									MetricName: "metric2",
+								},
+							},
+						},
+					},
+				},
+			},
+			Expected: &platformv1.CapsuleSpec{
+				Scale: &platformv1.Scale{
+					Horizontal: &platformv1.HorizontalScale{
+						CustomMetrics: []*v1alpha2.CustomMetric{
+							{
+								InstanceMetric: &v1alpha2.InstanceMetric{
+									MetricName: "metric1",
+								},
+							},
+							{
+								InstanceMetric: &v1alpha2.InstanceMetric{
+									MetricName: "metric3",
+								},
+							},
+						},
+					},
+				},
+			},
+			Changes: []Change{
+				{
+					FieldPath: "$.scale.horizontal.customMetrics[1]",
+					FieldID:   "$.scale.horizontal.customMetrics[1]",
+					Operation: RemovedOperation,
+				},
+				{
+					FieldPath: "$.scale.horizontal.customMetrics[1]",
+					FieldID:   "$.scale.horizontal.customMetrics[1]",
+					From: Value{
+						AsString: "",
+						Type:     NullType,
+					},
+					To: Value{
+						AsString: "instanceMetric:\n    metricName: metric3\n",
+						Type:     MapType,
+					},
+					Operation: AddedOperation,
+				},
+			},
+		},
+	}
+
+	runTests(t, tests)
+}
+
+func Test_Run_All(t *testing.T) {
+	t.Run("Interfaces", Test_Apply_Interfaces)
+	t.Run("Routes", Test_Apply_Routes)
+	t.Run("Env Vars", Test_Apply_Env_Vars)
+	t.Run("Primary Fields", Test_Apply_Primary_Fields)
+	t.Run("Args", Test_Apply_Args)
+	t.Run("Scale", Test_Apply_Scale)
+}
+
+func runTests(t *testing.T, tests []test) {
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
 			result, err := Apply(test.Base, test.Changes)
+			spec := result.(*platformv1.CapsuleSpec)
 			require.NoError(t, err)
-			d, err := Compare(result, test.Expected)
+			d, err := Compare(spec, test.Expected)
+			require.Empty(t, d.Changes)
 			require.NoError(t, err)
-
 			require.True(t, proto.Equal(test.Expected, result), d.Changes)
 		})
 	}
