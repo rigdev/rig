49ea8498c51242404c037c5f7beb9b462df02bd8
Anders Johnsen <anders@rig.dev>
2024-08-02 10:42:51 +0200

    🔖 Tag new charts (#1132)

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "1.10.4";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.27";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.40";
+export const RIG_VERSION = "1.11.0";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.31";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.43";
