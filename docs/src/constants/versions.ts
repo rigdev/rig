e442c4b7f78c0973b1eab8cbd29ada16a44cdb7c
Anders Johnsen <anders@rig.dev>
2024-08-16 16:17:39 +0200

    Update chart version

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
 export const RIG_VERSION = "1.11.3";
 export const RIG_OPERATOR_CHART_VERSION = "1.0.34";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.45";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.46";
