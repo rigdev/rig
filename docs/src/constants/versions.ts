fb768e2e7c6a5fe408023a446af248af38c48bc3
Matias Frank Jensen <matias@rig.dev>
2024-10-07 14:39:08 +0200

    :memo: Update rig versions in doc

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "1.12.2";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.40";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.53";
+export const RIG_VERSION = "1.12.3";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.41";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.54";
