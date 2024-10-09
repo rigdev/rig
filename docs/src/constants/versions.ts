606b15781ec0e73765db4cfbb1e11d5887451551
Matias Frank Jensen <matias@rig.dev>
2024-10-09 13:30:20 +0200

    :memo: Update version constants

--- docs/src/constants/versions.ts
+++ docs/src/constants/versions.ts
@@ -1,3 +1,3 @@
-export const RIG_VERSION = "1.12.3";
-export const RIG_OPERATOR_CHART_VERSION = "1.0.41";
-export const RIG_PLATFORM_CHART_VERSION = "1.0.54";
+export const RIG_VERSION = "1.12.4";
+export const RIG_OPERATOR_CHART_VERSION = "1.0.42";
+export const RIG_PLATFORM_CHART_VERSION = "1.0.55";
